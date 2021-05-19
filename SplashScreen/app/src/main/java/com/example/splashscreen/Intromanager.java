8d97d81b0abc57cf087324a4d247b44d4f7449a1
Taha Junaid <taha.junaid@futureapi.com>
2021-05-20 02:25:47 +0530

    Splash Screen with Sliders

---
+++ SplashScreen/app/src/main/java/com/example/splashscreen/Intromanager.java
@@ -0,0 +1,25 @@
+package com.example.splashscreen;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Intromanager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context context;
+    public Intromanager(Context context)
+    {
+        this.context=context;
+        pref=context.getSharedPreferences("first",0);
+        editor = pref.edit();
+    }
+    public void setFirst(boolean isFirst)
+    {
+        editor.putBoolean("check",isFirst);
+        editor.commit();
+    }
+    public boolean Check()
+    {
+        return pref.getBoolean("check",true);
+    }
+}
